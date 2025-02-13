0af875461a6ea2bf2d428a7294f137cff105eedc
amughal <amughal2@nait.ca>
2025-02-12 18:27:55 -0700

    privacypolicyupdated

--- app/privacy-policy/page.jsx
+++ app/privacy-policy/page.jsx
@@ -3,103 +3,106 @@ import Footer from "@/components/footer/footer";
 export default function PrivacyPolicy() {
   return (
     <>
-      <div
-        style={{
-          zIndex: -1,
-          position: "fixed",
-          width: "100vw",
-          height: "100vh",
-          top: 0,
-          left: 0,
-        }}
-      ></div>
-      <div
-        className="!pt-60 md:!pt-72"
-        style={{
-          position: "relative",
-          zIndex: 10,
-          maxWidth: "1200px",
-          margin: "auto",
-          padding: "2rem",
-          color: "black",
-        }}
-      >
-        <h1 className="text-3xl font-bold mb-6">Privacy Policy</h1>
-        <p>
-          <strong>Effective Date:</strong> 2 January 2025
+      <div className="max-w-screen-lg mx-auto pt-24 md:pt-40 px-4">
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-3xl font-bold text-[#201a7a]">Privacy Policy</h1>
+          <p className="text-2xl font-bold text-[#201a7a]">Effective Date: 2 January 2025</p>
+        </div>
+
+        <p className="mb-2">
+          <strong className="text-2xl font-bold text-red-700">Kinetic Data Minds Inc</strong> (“we,” “our,” or “us”) respects your privacy and is committed to protecting your
+          personal data. We comply with the Personal Information Protection and Electronics Document Act (PIPEDA) regarding data collection, use, and protection.
+          Under PIPEDA, you have the right to access, correct, or delete your personal data. You may also withdraw consent for data collection and usage.
+          To request this, contact us at <a href="mailto:aamir.mughal@kineticdataminds.ca" className="text-blue-500">aamir.mughal@kineticdataminds.ca</a> with the subject line: ‘Data Access Request’.
+          We will process your request within 30 days, subject to legal or security restrictions.
         </p>
-        <p>
-        Thank you for choosing Kinetic Data Minds Inc! This privacy policy 
-        outlines how we collect, use, share, and protect your personal information 
-        when you use our mobile application. By using the Construction Safety Mobile
-        App, you agree to the terms described in this policy. We are committed to 
-        safeguarding your privacy and ensuring the security of your personal information.
-         If you have any questions or concerns regarding the privacy policy, 
-         please contact us at <a a href="mailto:aamir.mughal@kineticdataminds.ca" 
-         class="email-link">aamir.mughal@kineticdataminds.ca</a>.
-           
+
+        <p className="mb-2">
+          If you have concerns about our data practices, you can file a complaint with us at <a href="mailto:aamir.mughal@kineticdataminds.ca" className="text-blue-500">aamir.mughal@kineticdataminds.ca</a>.
+          If unresolved, you may escalate your complaint to the <strong>Office of the Privacy Commissioner of Canada (OPC)</strong>:<br />
+          📌 Website: <a href="https://www.priv.gc.ca" className="text-blue-600">www.priv.gc.ca</a> <br />
+          📌 Phone: 1-800-282-1376
         </p>
-        <h2 className="text-2xl font-bold mt-4">Information We Collect</h2>
-        <p>
-          Personal information: We may collect personally identifiable 
-          information such as your name, email address, phone number, 
-          and any other information you voluntarily provide.
-        </p>
-        <p>
-        Location Data: We collect location data solely to verify that
-         you are logging in from a designated construction or work area. 
-         This ensures compliance with worksite requirements and app 
-         functionality. 
-        </p>
-        <p>
-        - Location data is only collected during login and is not continuously tracked or stored.
-        </p>
-        <p>
-        - You can disable location access in your device settings, but this may restrict access to certain app features. 
-        </p>
-        <h2 className="text-2xl font-bold mt-4">How We Use Your Information</h2>
-        <ul>
-          <li>
-            <strong>Personal Information:</strong> To provide a personalized experience, including user authentication and content delivery.  
-            <strong>Location Data:</strong> To confirm your location at a designated construction or work area during login, ensuring compliance with worksite requirements. 
-          </li> 
+
+        <section className="grid grid-cols-1 md:grid-cols-1 gap-6">
+          {/* Information We Collect */}
+          <div className="border p-4">
+            <h2 className="text-2xl font-bold underline mb-2 text-[#201a7a]">Information We Collect:</h2>
+            <ul className="list-disc pl-5">
+              <li><strong className="font-bold">FLHA (Field Level Hazard Assessments):</strong> Manually collected from users to monitor and document hazard assessments for safety compliance.</li>
+              <li><strong>Site Injury Reports:</strong> Manually submitted by users to document incidents and ensure appropriate follow-up actions.</li>
+              <li><strong>Work Permits:</strong> Users can upload, modify, and delete permits to manage access to construction sites and ensure safety compliance.</li>
+              <li><strong>Emergency Contacts:</strong> Provided to the User for reporting emergency situations.</li>
+            </ul>
+          </div>
+
+          {/* How We Use Your Information */}
+          <div className="border p-2">
+            <h2 className="text-2xl font-bold underline mb-2 text-[#201a7a]">How We Use Your Information:</h2>
+            <ul className="list-disc pl-5">
+              <li><strong>Enable Location:</strong> Ensures login is done at construction/work sites.</li>
+              <li><strong>Safety Incident Documentation:</strong> Helps analyze and document safety incidents.</li>
+              <li><strong>Work Permit Verification:</strong> Verifies user certifications for job roles and ensures compliance with safety and permit requirements.</li>
+              <li><strong>Emergency Alerts:</strong> Enables management to communicate with users during critical/emergency incidents.</li>
+             
+            </ul>
+          </div>
+
+          {/* How We Share Your Information */}
+          <div className="border p-2">
+            <h2 className="text-2xl font-bold underline mb-2 text-[#201a7a]">How We Share Your Information:</h2>
+            <ul className="list-disc pl-5">
+              <li><strong>Emergency Situations:</strong> Emergency contact information may be shared to ensure user safety.</li>
+              <li><strong>Location Data:</strong> <ul><li>User consent is mandatory to verify access to worksites.</li>
+              <li>Location data is not stored and is used only during the login verification process.</li>
+              <li>Location data is strictly for verification purposes and is not retained after login</li></ul></li>
+            </ul>
+          </div>
+
+          {/* Data Security */}
+          <div className="border p-1">
+            <h2 className="text-2xl font-bold underline mb-2 text-[#201a7a]">Data Security:</h2>
+            <ul className="list-disc pl-5 mb-4">
+          <li>We take data security seriously and implement industry-standard measures to protect your data from unauthorized access, alteration, disclosure, or destruction.</li>
+          <li>Despite our best efforts, no method of data transmission or storage is 100% secure. However, we continuously improve our security protocols to protect your information.</li>
+          <li>We do not sell or share personal data with third parties for advertising.</li>
+          <li>We may use third-party services (cloud storage, analytics) to enhance app functionality. These services comply with strict security standards.</li>
         </ul>
-        <h2 className="text-2xl font-bold mt-4">
-          How We Share Your Information
-        </h2>
+          </div>
+        </section>
+
+        {/* Data Disclosure */}
+        <h2 className="text-2xl font-bold mt-4 text-[#201a7a]">Data Disclosure – We Only Share Data When Legally Required</h2>
+        <ul className="list-disc pl-5 mb-4">
+          <li>In response to court orders, subpoenas, or regulatory investigations.</li>
+          <li>To prevent fraud, security threats, or illegal activities.</li>
+          <li>To comply with financial, tax, or employment laws.</li>
+        </ul>
+
+         {/* Data Retention */}
+         <h2 className="text-2xl font-bold mt-6 text-[#201a7a]">Data Retention:–We retain your data only as long as necessary to provide our services, ensure safety compliance, and meet legal obligations. </h2>
+        <ul className="list-disc pl-5 mb-4">
+          <li>Inactive Accounts: After 90 days of inactivity, we permanently delete personal data, except where retention is required by law. Users may receive a notification before deletion.</li>
+          <li>Manual Deletion Requests: You may request data deletion at any time by contacting aamir.mughal@kineticdataminds.ca. We will process your request within 30 days.</li> 
+        </ul> 
+
+          {/* Data Retention */}
+          <h2 className="text-2xl font-bold mt-6 text-[#201a7a]">Your Rights:–Under PIPEDA, you have the right to: </h2>
+        <ul className="list-disc pl-5 mb-4">
+          <li>Access Your Data: Request a copy of your personal data stored by us.</li>
+          <li>Correct Inaccurate Data: Request updates or corrections to your information</li> 
+          <li>Withdraw Consent: Revoke consent for data collection (this may limit app functionality).</li>
+          <li>Request Data Deletion: Ask us to delete your data, subject to legal and security constraints.</li>
+          <li>Lodge a Complaint: Report concerns about data handling to us or the Office of the Privacy Commissioner of Canada.</li>
+        </ul> 
+
+        {/* Contact Info */}
+        <h2 className="text-2xl font-bold mt-6 text-[#201a7a]">Contact Us</h2>
         <p>
-          <strong>Personal Information :</strong> We may disclose your personal information if 
-          required by law, court order, or government authority through legal means.  
-        </p>
-        <p>
-          <strong>Location Data :</strong> :We do not sell or share your location data with third parties. 
-          Location data is used strictly for login verification purposes and is not retained after verification  
-        </p>
-        <h2 className="text-2xl font-bold mt-4">Data Security</h2>
-        <p>
-          We take data security seriously and employ reasonable measures to
-          protect your information from unauthorized access, alteration,
-          disclosure, or destruction. Despite our best efforts, no method of
-          data transmission over the internet or electronic storage is entirely
-          secure. Therefore, we cannot guarantee the absolute security of your
-          data.
-        </p>
-        <p>
-        - Location data is not stored and is only used momentarily during the login verification 
-        </p>
-        <h2 className="text-2xl font-bold mt-4">Contact Us</h2>
-        <p>
-          If you have any questions or concerns about this privacy policy or our
-          data practices, please contact us at:{" "}
-          <a href="mailto:aamir.mughal@kineticdataminds.ca" class="email-link">aamir.mughal@kineticdataminds.ca</a>.
-        </p>
-        <p className="mt-4">
-          By using the Construction Safety Mobile application, you agree to the
-          terms of this privacy policy and never share your login credentials
-          with any person. Thank you for trusting us with your information and
-          for using Construction Safety Mobile Application.
+          For any privacy concerns, contact us at: <a href="mailto:aamir.mughal@kineticdataminds.ca" className="text-blue-500">aamir.mughal@kineticdataminds.ca</a> or call +1-(587) 990-6399.
         </p>
       </div>
+
       <Footer />
     </>
   );
