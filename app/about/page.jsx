1498b885ff85373bcd39e486a549bf6d71be0ec1
amughal <amughal2@nait.ca>
2025-02-25 09:17:16 -0700

    track tag removedd

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -6,7 +6,6 @@ import Footer from "@/components/footer/footer";
 import clsx from "clsx";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import Script from "next/script";
 
 const roboto = Kanit({
   subsets: ["latin"],
@@ -27,20 +26,6 @@ export default function Home() {
 
   return (
     <>
-     {/* Google Tag Manager */}
-     <Script
-        id="google-tag-manager"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-XXXXXXX');
-          `,
-        }}
-      />
       <div
         style={{
           zIndex: -1,
