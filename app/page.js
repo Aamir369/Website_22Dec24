fed650782976fb4f962b50b045a8636641142da7
amughal <amughal2@nait.ca>
2025-02-25 08:38:30 -0700

    track tag added

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Poppins } from "next/font/google";
 import Footer from "@/components/footer/footer";
 import styles from "../css/globals.css";
-
+import Script from "next/script";
 const roboto = Poppins({
   subsets: ["latin"],
   weight: ["400", "700"],
@@ -10,7 +10,23 @@ const roboto = Poppins({
 
 export default function Home() {
   return (
+
+    
     <>
+     {/* Google Tag Manager */}
+     <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-XXXXXXX');
+          `,
+        }}
+      />
       <main className="relative w-full h-screen overflow-hidden">
         {/* Background Video */}
         <video
